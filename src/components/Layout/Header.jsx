84b86a6dacee1cbeb0057c2caaab52b8bcb73d66
Raymart <raymartformalejo00@gmail.com>
2023-02-09 09:22:19 +0800

    init

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header>
+        <h1>ReactMeals</h1>
+        <button>Cart</button>
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
